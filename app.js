20272f111fe66496ca858a967974da1ea04f2efb
code@jasom.eu <site@arxa.eu>
2024-05-08 22:23:21 +0200

    autocomplete

--- app.js
+++ app.js
@@ -13,7 +13,71 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Hello World3!' });
-})
+});
+
+app.get('/getvalues/:id/:filter', (req, res) => {
+
+  N = 5;
+  const id = req.params.id;
+  const filter = decodeURIComponent(req.params.filter);
+  console.log(id, filter);
+  let source = [];
+
+  switch (id) {
+    case 'city':
+      source = [
+        { "label": "Bratislava", "key": "BA" },
+        { "label": "Senec", "key": "SC" },
+        { "label": "Trnava", "key": "TT" },
+        { "label": "Poprad", "key": "PP" },
+        { "label": "Nitra", "key": "NR" },
+        { "label": "Žilina", "key": "ZA" },
+        { "label": "Košice", "key": "KE" },
+        { "label": "Rožňava", "key": "RV" }
+      ];
+      break;
+    case 'color':
+      source = [
+        { "label": "Red", "key": "R" },
+        { "label": "Green", "key": "G" },
+        { "label": "Blue", "key": "B" },
+        { "label": "Yellow", "key": "Y" },
+        { "label": "Magenta", "key": "M" },
+        { "label": "Brown", "key": "BR" },
+        { "label": "White", "key": "W" },
+        { "label": "Black", "key": "BL" }
+      ];
+
+      break;
+
+    default:
+      break;
+  }
+
+  let result = [];
+
+  if (filter !== '-') {
+    const normFilter = removeDiacritics(filter)
+
+    let cnt = 0;
+    for (const item of source) {
+      if (removeDiacritics(item.label).includes(normFilter)) {
+        result.push(item);
+        cnt++;
+        if (cnt >= N) {
+          break;
+        }
+      }
+    }
+
+    // result = source.filter(i => removeDiacritics(i.label).includes(normFilter));
+  } else {
+    result = source;
+  }
+
+  result.sort((a, b) => a.label.localeCompare(b.label));
+  res.json(result);
+});
 
 app.put('/save-json/:id', (req, res) => {
   const id = req.params.id;
@@ -35,3 +99,9 @@ app.put('/save-json/:id', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+function removeDiacritics(str) {
+  if (str)
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+  return str;
+}
